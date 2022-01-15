c5f9a38c9040ed0f8969ec24c1d35784e679d67d
Arina <arinameder123@gmail.com>
2022-01-15 15:19:29 +0700

    add hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,20 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
     new WOW().init();
-});
+
+    const promoMenu = document.querySelector('.promo__menu'),
+          hamburger = document.querySelector('.hamburger'),
+          menuItem = document.querySelectorAll('.promo__menu__item')
+
+    hamburger.addEventListener('click', () => {
+        promoMenu.classList.toggle('promo__menu_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            promoMenu.classList.toggle('promo__menu_active');
+            hamburger.classList.toggle('hamburger_active')
+        });
+    });
+}); 
